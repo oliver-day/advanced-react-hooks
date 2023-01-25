5b157b57c08c21799752d13076ddb629e6425b78
Oliver Day <oliver.day.swe@gmail.com>
2023-01-24 20:23:05 -0800

    feat: add 'obj or fun' solution

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,7 +5,7 @@ import * as React from 'react';
 
 const countReducer = (state, action) => ({
   ...state,
-  ...action
+  ...(typeof action === 'function' ? action(state) : action),
 });
 
 function Counter({ initialCount = 0, step = 1 } = {}) {
@@ -14,9 +14,10 @@ function Counter({ initialCount = 0, step = 1 } = {}) {
   });
 
   const { count } = state;
-  const increment = () => setState({
-    count: count + step,
-  });
+  const increment = () => setState(
+    currentState =>
+      ({ count: currentState.count + step })
+  );
 
   return <button onClick={increment}>{count}</button>;
 }
