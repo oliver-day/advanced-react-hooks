8d443b58866a15802909bb79251b5b37880ec6dc
Oliver Day <oliver.day.swe@gmail.com>
2023-01-25 19:17:01 -0800

    feat: add 'useCallback for memoization' solution

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -27,7 +27,7 @@ function asyncReducer(state, action) {
   }
 }
 
-function useAsync(asyncCallback, initialState, dependencies) {
+function useAsync(asyncCallback, initialState) {
   const [state, dispatch] = React.useReducer(asyncReducer, {
     status: 'idle',
     data: null,
@@ -44,19 +44,20 @@ function useAsync(asyncCallback, initialState, dependencies) {
     promise
       .then(data => dispatch({ type: 'resolved', data }))
       .catch(error => dispatch({ type: 'rejected', error }))
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencies);
+  }, [asyncCallback]);
 
   return state;
 }
 
 function PokemonInfo({pokemonName}) {
-  const state = useAsync(() => {
+  const asyncCallback = React.useCallback(() => {
     if (!pokemonName) {
       return
     }
     return fetchPokemon(pokemonName)
-  },
+  }, [pokemonName]);
+  
+  const state = useAsync(asyncCallback,
   { status: pokemonName ? 'pending' : 'idle' },
   [pokemonName]);
   
